ac83a7ead9d08e1c35013538d3955439bcd218ce
Stephanie Schneider <sschneider713@gmail.com>
2013-06-18 12:58:21 -0400

    scrolling

--- js/trial.js
+++ js/trial.js
@@ -4,25 +4,20 @@
 // option 2
 // every second get a number of events, add them to histogram
 
+
+
 window.onload = function(){
 		
 	call_with_random();
 	var interval = setInterval(call_with_random, 1000);
-	
-	document.getElementById("stop_button").onclick = function (){
-		interval = clearInterval(interval);		
-		
-		};
-		
-		$('#container').jscroll();
-	
+
 };
 
 
 function add(num){
 
 	var container = document.getElementById("container");
-	
+	var current = container.childNodes;
 	var time = new Date();
 	
 	var piece = document.createElement("span");
@@ -31,9 +26,11 @@ function add(num){
 	piece.style.height = (num * 5) + "px";
 	piece.onclick = send;
 		//piece.style = "height: " + num * 10 + "px;";
-	
- container.appendChild(piece);
 		
+	
+	
+ 	container.insertBefore(piece, current[0]);
+	
 	}
 	
 	function call_with_random(){

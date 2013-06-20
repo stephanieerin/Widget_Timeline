4949e7aa5153c92d9dffc68cfaff843175793c57
Stephanie Schneider <sschneider713@gmail.com>
2013-06-20 15:01:21 -0400

    add to next century, comment

--- js/intents.js
+++ js/intents.js
@@ -1,10 +1,12 @@
 OWF.relayFile = '/owf-sample-html/js/eventing/rpc_relay.uncompressed.html';
 	
+	
+	// initialize subscription to channel
 function init() {
-	document.getElementById("message").innerHTML = new Date();
 	OWF.Eventing.subscribe("testChannel", this.add);		
 }
 		
+	// when information is received, add a bar to the timeline
 var add = function(sender, msg){
 		var num = parseInt(msg);
 		var container = document.getElementById("container");
@@ -23,21 +25,26 @@ var add = function(sender, msg){
 	 	count = count + 1;
 	 	document.getElementById("counter").innerHTML = count;
 	 	
-	 	OWF.Eventing.publish("testChannel", count);
+//	 	OWF.Eventing.publish("testChannel", count);
 };
 		
+ // owfdojo stuff (when ready, call init)
 owfdojo.addOnLoad(function(){
 	OWF.ready(init);
 });
 
+	// old function used for testing (calls add with a random number)
 function call_with_random(){
 
 	var rand = Math.floor((Math.random() * 20) +1);
-	add(rand);
+	add(null, rand);
 		
 }
-		
+	//	send time information on click
 function send(){
 	console.log(this.time);
+	//OWF.Eventing.publish(this.time);
 	
+	// problem: subscribing to the channel means that if you publish to it you end
+	// up in an infinite loop. fix?
 }
